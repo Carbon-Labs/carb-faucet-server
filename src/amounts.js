3693145846a27da39f7ad508b75da13e56ed8cf1
anas <anastal2001@yahoo.com>
2022-01-27 08:22:04 +0100

    faucet new test tokens used in test swap

--- src/amounts.js
+++ src/amounts.js
@@ -6,31 +6,31 @@ module.exports = [
         perRequest: 5000
     },
     {
-        tokenAddress: "0xf3049f5ac8477b164ae80489655a56d9d86d781b",
+        tokenAddress: "0xc5c5f8786574522e83242e5981482d63c9ac7101",
         currency: "carb",
         decimals: 8,
         perAccount: 1000,
         perRequest: 100
     },
     {
-        tokenAddress: "0x6c3700b6e66a4aaeb113909b0b094af5394d8c84",
-        currency: "xsgd",
-        decimals: 6,
+        tokenAddress: "0x984219c0d7a9ebd54b433792232b69c99e78b538",
+        currency: "stream",
+        decimals: 8,
         perAccount: 25000,
         perRequest: 5000
     },
     {
-        tokenAddress: "0x10cad18c44fc0053365452e4661e31c16ca1f19d",
-        currency: "zwap",
-        decimals: 12,
-        perAccount: 500,
-        perRequest: 50
+        tokenAddress: "0x94677f06ad3046bd7c793fcb179ca79c822e6dd5",
+        currency: "xcad",
+        decimals: 8,
+        perAccount: 1000,
+        perRequest: 100
     },
     {
-        tokenAddress: "0x7a3793c0c825d8c335dd2eb6f53e41d451503bf0",
-        currency: "gzil",
-        decimals: 15,
-        perAccount: 500,
-        perRequest: 50
+        tokenAddress: "0xc2e8a69f162d59935430eb936467f23ff4091f27",
+        currency: "grph",
+        decimals: 8,
+        perAccount: 1000,
+        perRequest: 100
     },
 ];
